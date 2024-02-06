d52fa5bcf4126572c1f7484d6cfd65ea78571662
C4r4cole <c4r4cole.chevalerie@gmail.com>
2024-02-06 19:42:02 +0100

    config tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./assets/**/*.{html,js}"],
+  theme: {
+    colors: {
+      yellow: "#FFD15B",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
