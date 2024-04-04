d490ab140f0c12be3be1dad4965e5d108a94fbb1
C4r4cole <c4r4cole.chevalerie@gmail.com>
2024-04-04 10:10:56 +0200

    Project fix

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,53 +1,40 @@
+import { createIngredientName, createIngredientQtyUnit, createIngredientQuantity, createIngredientUnit } from "./ingredient.js";
 import {recipes} from "./recipes.js";
 
-const ingredients = [];
-const appareilsTable = [];
-const ustensilesTable = [];
 
-
-function getTrimmedDescription(description, descriptionDiv){
-    const maxChars = 200;
-    if (description.length > maxChars) {
-        const lastSpaceIndex = description.lastIndexOf(" ", maxChars);
-        const trimmedDescription = description.slice(0, lastSpaceIndex);
-        descriptionDiv.textContent = trimmedDescription;
-    } else {
-        descriptionDiv.textContent = description;
-    }
-};
-
-function deleteDuplicates(ingredientsTable){
-    for (const element of ingredientsTable){
-        if (!ingredients.includes(element.ingredient)){
-            ingredients.push(element.ingredient);
+function pushWithoutDuplicates(dropDownTable, string){
+        for (const element of dropDownTable){
+            if (element.localeCompare(string, undefined, {sensitivity: 'base'}) === 0){
+                return
+            }
         }
-    }
+        dropDownTable.push(string)
 }
 
+function fillDefaultTable(recipe){
+    for (const {ingredient} of recipe.ingredients){
+        pushWithoutDuplicates(INGREDIENTS, ingredient)
+    }
+
+        pushWithoutDuplicates(APPAREILS, recipe.appliance);
+
+
+        for (const ustensil of recipe.ustensils){
+        pushWithoutDuplicates(USTENSILES, ustensil);
+
+        }
+};
 
 function displayIngredient(ingredientsTable, ingredientsGrid){
+    const ingredientQtyUnit = createIngredientQtyUnit();
     for (const element of ingredientsTable){
-
-        deleteDuplicates(ingredientsTable);
     
-        const ingredientCard = document.createElement("div");    
-        const ingredientName = document.createElement("h4");
-        const ingredientQty = document.createElement("p");
-        const ingredientUnit = document.createElement("span");
-        const ingredientQtyUnit = document.createElement("div");
+        const ingredientCard = document.createElement("div"); 
     
-        ingredientQtyUnit.classList.add("flex");
-        ingredientName.textContent = element.ingredient;
-        ingredientName.classList.add("text-sm", "font-manrope");
-        ingredientQty.textContent = element.quantity;
-        ingredientQty.classList.add("text-sm", "font-manrope", "text-grey2");
-        ingredientUnit.textContent = element.unit;
-        ingredientUnit.classList.add("text-sm", "font-manrope", "text-grey2");
-    
-        ingredientQtyUnit.appendChild(ingredientQty);
-        ingredientQtyUnit.appendChild(ingredientUnit);
-        ingredientCard.appendChild(ingredientName);
-        ingredientCard.appendChild(ingredientQtyUnit);
+        ingredientCard.appendChild(createIngredientQtyUnit());
+        ingredientCard.appendChild(createIngredientName(element));
+        ingredientQtyUnit.appendChild(createIngredientQuantity(element));
+        ingredientQtyUnit.appendChild(createIngredientUnit(element));
     
         ingredientsGrid.appendChild(ingredientCard);
     }
@@ -57,7 +44,7 @@ function displayRecipe(){
     for (const element of recipes){
 
         const imgSrc = element.image;
-        const ingredientsTable = element.ingredients;
+        const ingredients = element.ingredients;
         const description = element.description;
         
         const recipesGrid = document.querySelector(".grid-content");
@@ -68,14 +55,14 @@ function displayRecipe(){
         const title = document.createElement("h2");
         const recette = document.createElement("h3");
         const descriptionDiv = document.createElement("p");
-        const ingredients = document.createElement("h3");        
+        const ingredientsDiv = document.createElement("h3");        
         
         card.classList.add("rounded-2xl", "bg-white", "h-[700px]");
         recipe.classList.add("rounded-b-2xl", "bg-white", "p-4");
         title.classList.add("font-anton", "my-6", "text-lg");
         recette.classList.add("font-manrope", "mb-2", "text-xs", "font-bold", "text-grey2");
-        ingredients.classList.add("font-manrope", "mb-2", "text-xs", "font-bold", "text-grey2");
-        descriptionDiv.classList.add("font-manrope", "bg-white", "mb-6", "text-sm");
+        ingredientsDiv.classList.add("font-manrope", "mb-2", "text-xs", "font-bold", "text-grey2");
+        descriptionDiv.classList.add("line-clamp-5", "font-manrope", "bg-white", "mb-6", "text-sm");
         ingredientsGrid.classList.add("rounded-b-2xl");
         
         img.setAttribute("src", `./images/recettes/${imgSrc}`);
@@ -85,61 +72,31 @@ function displayRecipe(){
         title.textContent = element.name;
         recette.textContent = "recette";
         recette.classList.add("uppercase");
-        ingredients.textContent = "ingrédients";
-        ingredients.classList.add("uppercase");
+        ingredientsDiv.textContent = "ingrédients";
+        ingredientsDiv.classList.add("uppercase");
         ingredientsGrid.classList.add("grid", "grid-cols-2", "gap-x-4", "gap-y-4");
-        
+        descriptionDiv.textContent = description;
     
         card.appendChild(img);
         card.appendChild(recipe);
         recipe.appendChild(title);
         recipe.appendChild(recette);
         recipe.appendChild(descriptionDiv);
-        recipe.appendChild(ingredients);
+        recipe.appendChild(ingredientsDiv);
         recipe.appendChild(ingredientsGrid);
         recipesGrid.appendChild(card);
-        
-        displayIngredient(ingredientsTable, ingredientsGrid);
 
-        getTrimmedDescription(description, descriptionDiv);
-        
-
-        
-        
-
-    
-        if (!appareilsTable.includes(element.appliance)){
-            appareilsTable.push(element.appliance);
-        }
-    
+        displayIngredient(ingredients, ingredientsGrid);
+        fillDefaultTable(element);
     }
-}
-
-displayRecipe();
-
+};
 
 function getDropdownElementsById(id){
     const wrapper = document.getElementById(id);
     const input = wrapper.querySelector("input");
     const list = wrapper.querySelector("ul");
     return {wrapper, input, list};
-}
-
-const ingredientElements = getDropdownElementsById("ingredients");
-const appareilsElements = getDropdownElementsById("appareils");
-const ustensilesElements = getDropdownElementsById("ustensiles");
-
-
-    ingredientElements.input.addEventListener("input", (e)=>{
-        filterBy(ingredients, ingredientElements.list, e.target.value);
-    })
-    appareilsElements.input.addEventListener("input", (e)=>{
-        filterBy(appareilsTable, appareilsElements.list, e.target.value);
-    })
-    ustensilesElements.input.addEventListener("input", (e)=>{
-        filterBy(ustensilesTable, ustensilesElements.list, e.target.value);
-    })
-
+};
 
 function filterBy(list, element, userInput){
     const finalList = [];
@@ -149,7 +106,7 @@ function filterBy(list, element, userInput){
         }
     }
     addDropdownElements(element, finalList);
-}
+};
 
 
 function addDropdownElements(parentElement, elements){
@@ -157,16 +114,47 @@ function addDropdownElements(parentElement, elements){
     for (const element of elements){
         const li = document.createElement("li");
         li.textContent = element;
+        li.classList.add("mb-5", "hover:bg-yellow");
         parentElement.appendChild(li);
     }
+};
+
+function setRecipesNumber(recipeTab){
+    const recipeNb = document.getElementById("recipe-nb");
+    recipeNb.textContent = recipeTab.length;
 }
 
-addDropdownElements(ingredientElements.list, ingredients);
-addDropdownElements(appareilsElements.list, appareilsTable);
-addDropdownElements(ustensilesElements.list, ustensilesTable);
+const INGREDIENTS = [];
+const APPAREILS = [];
+const USTENSILES = [];
+
+
+const ingredientsElements = getDropdownElementsById("ingredients");
+const appareilsElements = getDropdownElementsById("appareils");
+const ustensilesElements = getDropdownElementsById("ustensiles");
+
+
+ingredientsElements.input.addEventListener("input", (e)=>{
+        filterBy(INGREDIENTS, ingredientsElements.list, e.target.value);
+    })
+    appareilsElements.input.addEventListener("input", (e)=>{
+        filterBy(APPAREILS, appareilsElements.list, e.target.value);
+    })
+    ustensilesElements.input.addEventListener("input", (e)=>{
+        filterBy(USTENSILES, ustensilesElements.list, e.target.value);
+    })
+    
+    
+addDropdownElements(ingredientsElements.list, INGREDIENTS);
+addDropdownElements(appareilsElements.list, APPAREILS);
+addDropdownElements(ustensilesElements.list, USTENSILES);
 
 
 
 // setTimeout(() => {
-//     addDropdownElements(dropdownIngredientsList, []);
-// }, 3000);
+    //     addDropdownElements(dropdownIngredientsList, []);
+    // }, 3000);
+    
+    
+    setRecipesNumber(recipes);
+    displayRecipe();
